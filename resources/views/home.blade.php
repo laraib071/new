6daa9631063f94a516382e85e4f384192b07bf41
Abdul786060 <rafayabdul252@gmail.com>
2019-11-09 21:25:10 +0500

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/software.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+@yield('content')
+@if(Request::is('about','/'))
+<div class="container p-5 bg-primary">
+<h1 >Abdul Rafay Javed.</h1>
+<h1>FA17-BSE-071.</h1>
+<h1>FA17-BSE-5B.</h1>
+</div>
+@endif
+
+@if(Request::is('contact','/'))
+<div class="container p-5 bg-danger">
+<input type="text" placeholder="enter a message"/><br>
+<input type="text" placeholder="enter a name "/><br>
+<input type="button" value="Done"/>
+
+@endif
+</div>
+</body>
+</html>
